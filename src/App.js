27f3f8e8ec47bb98333b013740d19233a6a3ae92
niwatoriDev <esmunozdev@gmail.com>
2022-06-02 18:45:58 -0500

    Changed color pallete and added Personal Info Card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import MainSec from './components/MainSec/MainSec'
 import InfoCard from './components/InfoCard/InfoCard'
 import Portfolio from './components/Portfolio/Portfolio'
 import './App.css'
+import {Heading} from '@chakra-ui/react'
+import homeIcon from "./images/icons/home.png"
 
 function App() {
   return (
@@ -14,8 +16,20 @@ function App() {
       <Route path='info' element={<InfoCard/>}/>
       <Route path='portfolio' element={<Portfolio/>}/>
     </Routes>
+    <div className="part3">
+            <Heading size='2xl' id="nameFull"> 
+                Esteban Munoz
+            </Heading>
+            <Heading size='xs' id="alias"> 
+                esmunozdev
+            </Heading>
+            <Link to="/"><img src={homeIcon} alt='home-icon' className='homeIcon'/></Link>
+        </div>
   </div>
   );
 }
+
+
+
 
 export default App;
--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -2,13 +2,39 @@ import React from "react";
 import "./InfoCard.css"
 import { Heading } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import profPic from "../../images/Circular.png"
 
 
 const InfoCard = () => {
     return (
     <div className="main-Sec">
-<Heading>Under Construction</Heading>
-        <Link to="/">Go Back</Link>
+        <div className="Main-Div-Info">
+            <div className="Info-1">
+                <div>
+                    <Heading size='lg' id="textInfo">+52 2223900810</Heading>   
+                </div>
+                <div>
+                    <Heading size='lg' id="textInfo">esmunozdev@gmail.com</Heading>   
+                </div>
+                <div>
+                    <Heading size='lg' id="textInfo">México</Heading>   
+                </div>
+            </div>
+            <div className="Info-2">
+                    <img src={profPic} alt="prof-pic" className="profPic"/>
+            </div>
+            <div className="Info-3">
+                <div>
+                    <Heading size='lg' id="textInfo">Digital Native</Heading>   
+                </div>
+                <div>
+                    <Heading size='lg' id="textInfo">Front-end Passionated</Heading>   
+                </div> 
+                <div>   
+                    <Heading size='lg' id="textInfo">México</Heading>   
+                </div>
+            </div>
+        </div>
     </div>
     );
   };
